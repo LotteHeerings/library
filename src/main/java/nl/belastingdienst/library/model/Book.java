ebf928137e6ab24a0a358cbf63a920852bd170dc
LotteHeerings <129155976+LotteHeerings@users.noreply.github.com>
2023-04-03 21:39:45 +0200

    Initial commit

---
+++ src/main/java/nl/belastingdienst/library/model/Book.java
@@ -0,0 +1,60 @@
+package nl.belastingdienst.library.model;
+
+import java.util.Date;
+
+public class Book {
+    private long ISBN;
+    private String title;
+    private String authors;
+    private String publisher;
+    private Date publication_date;
+    private String language;
+
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Date getPublication_date() {
+        return publication_date;
+    }
+
+    public void setPublication_date(Date publication_date) {
+        this.publication_date = publication_date;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
