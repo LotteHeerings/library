59e9759a8bccd8134f1e4c709358faeda52c1d70
LotteHeerings <129155976+LotteHeerings@users.noreply.github.com>
2023-04-06 17:15:45 +0200

    Update SecurityConfig.java

--- src/main/java/nl/belastingdienst/library/config/SecurityConfig.java
+++ src/main/java/nl/belastingdienst/library/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package nl.belastingdienst.library.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
