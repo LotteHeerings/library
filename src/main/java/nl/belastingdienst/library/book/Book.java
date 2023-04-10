851ec5357d4776b8df36c078222148facec14b7c
LotteHeerings <129155976+LotteHeerings@users.noreply.github.com>
2023-04-10 19:19:31 +0200

    minor text changes

--- src/main/java/nl/belastingdienst/library/book/Book.java
+++ src/main/java/nl/belastingdienst/library/book/Book.java
@@ -1,4 +1,43 @@
 package nl.belastingdienst.library.book;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_book")
 public class Book {
+    @Id
+    @Column(name = "ISBN-13", nullable = false, unique = true)
+    private Integer ISBN13;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "authors")
+    private String authors;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "cover_path")
+    private String cover_path;
+
+    @Column(name = "language")
+    private String language;
+
+    @Column(name = "publication_date")
+    private String publication_date;
 }
--- src/main/java/nl/belastingdienst/library/user/UserController.java
+++ src/main/java/nl/belastingdienst/library/user/UserController.java
@@ -7,29 +7,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/admin/users")
+    @PostMapping("/users")
     public User createUser(@RequestBody User user) throws Exception {
         return userService.createUser(user);
     }
 
 
-    @GetMapping("/admin/users")
+    @GetMapping("/users")
     public List<User> readUsers(){
         return userService.getAllUserTypes();
     }
 
-    @DeleteMapping("/admin/users/{email}")
+    @DeleteMapping("/users/{email}")
     public void deleteUser(@PathVariable(value = "email") String email) {
         userService.deleteUser(email);
     }
 
-    @PutMapping("/admin/users/{email}")
+    @PutMapping("/users/{email}")
     public User updateUser(@PathVariable(value = "email") String email, @RequestBody User userDetails) {
         return userService.updateUser(email, userDetails);
     }
