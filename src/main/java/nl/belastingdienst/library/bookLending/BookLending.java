8705100a1f6e217c7a8e8496a8d3e132d7965525
LotteHeerings <129155976+LotteHeerings@users.noreply.github.com>
2023-04-11 20:40:06 +0200

    Update BookLending.java

--- src/main/java/nl/belastingdienst/library/bookLending/BookLending.java
+++ src/main/java/nl/belastingdienst/library/bookLending/BookLending.java
@@ -1,4 +1,35 @@
 package nl.belastingdienst.library.bookLending;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nl.belastingdienst.library.book.Book;
+import nl.belastingdienst.library.user.User;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_book_lending")
 public class BookLending {
+    @Id
+    @OneToOne
+    @Column(name = "ISBN13")
+    private Book book;
+
+    @ManyToOne
+    @Column(name = "email")
+    private User user;
+
+    @Column(name = "return_before")
+    private LocalDate returnDate;
+
+    @Column(name = "date_of_handout")
+    private LocalDate handOutDate;
+
 }
