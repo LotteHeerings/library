b297d795c4db1c8c58caf573507f9c40ee4b7963
LotteHeerings <129155976+LotteHeerings@users.noreply.github.com>
2023-04-11 20:46:34 +0200

    Update BookLendingRepository.java

--- src/main/java/nl/belastingdienst/library/bookLending/BookLendingRepository.java
+++ src/main/java/nl/belastingdienst/library/bookLending/BookLendingRepository.java
@@ -1,4 +1,8 @@
 package nl.belastingdienst.library.bookLending;
 
-public interface BookLendingRepository {
+import nl.belastingdienst.library.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookLendingRepository extends JpaRepository<BookLending, String> {
+    Book findByISBN13(String ISBN13);
 }
