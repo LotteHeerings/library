5fc15c0fe7de7a5f8dbebccd644d46d5f184ffa1
LotteHeerings <129155976+LotteHeerings@users.noreply.github.com>
2023-04-11 20:45:09 +0200

    Update BookLendingDto.java

--- src/main/java/nl/belastingdienst/library/bookLending/BookLendingDto.java
+++ src/main/java/nl/belastingdienst/library/bookLending/BookLendingDto.java
@@ -1,4 +1,31 @@
 package nl.belastingdienst.library.bookLending;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookLendingDto {
+
+    @NotBlank
+    @Min(13)
+    private String ISBN13;
+
+    @NotBlank
+    @Max(64)
+    private String email;
+
+    @NotBlank
+    private String returnDate;
+
+    @NotBlank
+    private String handOutDate;
+
 }
